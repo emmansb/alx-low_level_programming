09b205d66844abddd721791be3ea7dc9fb340718
emmansb <biodunadekiitan@gmail.com>
2022-04-12 10:15:29 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
+ *@n: the int to print
  * Return: Always 0.
  */
-int main(void)
+int print_sign(int n)
 {
-	int r;
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
 
-	r = print_sign(98);
-	if (r < 0)
-		r *= -1;
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+else if (n == 0)
+{
+_putchar ('0');
+return (0);
+}
+
+else
+{
+_putchar ('-');
+return (-1);
+}
+
 }
